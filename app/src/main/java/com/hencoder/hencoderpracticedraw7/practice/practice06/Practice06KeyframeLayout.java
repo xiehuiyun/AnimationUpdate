48eefe293d879d2d13457cc27d0a53d955ca0ab6
NataliePortman <1401010800@qq.com>
2017-09-14 14:12:17 +0800

    finish PropertyValuesHolders.ofKeyframe()

--- app/src/main/java/com/hencoder/hencoderpracticedraw7/practice/practice06/Practice06KeyframeLayout.java
+++ app/src/main/java/com/hencoder/hencoderpracticedraw7/practice/practice06/Practice06KeyframeLayout.java
@@ -1,6 +1,10 @@
 package com.hencoder.hencoderpracticedraw7.practice.practice06;
 
+import android.animation.Keyframe;
+import android.animation.ObjectAnimator;
+import android.animation.PropertyValuesHolder;
 import android.content.Context;
+import android.inputmethodservice.Keyboard;
 import android.util.AttributeSet;
 import android.view.View;
 import android.widget.Button;
@@ -31,15 +35,25 @@ public class Practice06KeyframeLayout extends RelativeLayout {
         view = (Practice06KeyframeView) findViewById(R.id.objectAnimatorView);
         animateBt = (Button) findViewById(R.id.animateBt);
 
+        /*
+        使用 PropertyValuesHolder 来对多个属性同时做动画；
+        使用 AnimatorSet 来同时管理调配多个动画；
+        PropertyValuesHolder 的进阶使用：使用 PropertyValuesHolder.ofKeyframe() 来把一个属性拆分成多段，执行更加精细的属性动画。
+         */
         animateBt.setOnClickListener(new OnClickListener() {
             @Override
             public void onClick(View v) {
                 // 使用 Keyframe.ofFloat() 来为 view 的 progress 属性创建关键帧
                 // 初始帧：progress 为 0
+                Keyframe key1 = Keyframe.ofFloat(0, 0);
                 // 时间进行到一般：progress 为 100
+                Keyframe key2 = Keyframe.ofFloat(0.5f, 100);
                 // 结束帧：progress 回落到 80
+                Keyframe key3 = Keyframe.ofFloat(1, 80);
                 // 使用 PropertyValuesHolder.ofKeyframe() 来把关键帧拼接成一个完整的属性动画方案
+                PropertyValuesHolder holder = PropertyValuesHolder.ofKeyframe("progress", key1, key2, key3);
                 // 使用 ObjectAnimator.ofPropertyValuesHolder() 来创建动画
+                ObjectAnimator.ofPropertyValuesHolder(view, holder).setDuration(1000).start();
             }
         });
     }
