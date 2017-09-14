0ddfa3e5d6dffdf0246de53ebe6e8cd4a43e4f6e
NataliePortman <1401010800@qq.com>
2017-09-14 13:59:18 +0800

    finish AnimatorSet

--- app/src/main/java/com/hencoder/hencoderpracticedraw7/practice/Practice05AnimatorSetLayout.java
+++ app/src/main/java/com/hencoder/hencoderpracticedraw7/practice/Practice05AnimatorSetLayout.java
@@ -10,6 +10,10 @@ import android.widget.RelativeLayout;
 
 import com.hencoder.hencoderpracticedraw7.R;
 
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+
 public class Practice05AnimatorSetLayout extends RelativeLayout {
     View view;
     Button animateBt;
@@ -47,6 +51,8 @@ public class Practice05AnimatorSetLayout extends RelativeLayout {
                 // 用 AnimatorSet 的方法来让三个动画协作执行
                 // 要求 1： animator1 先执行，animator2 在 animator1 完成后立即开始
                 // 要求 2： animator2 和 animator3 同时开始
+                animatorSet.play(animator1);
+                animatorSet.playTogether(animator2,animator3);
 
                 animatorSet.start();
             }
